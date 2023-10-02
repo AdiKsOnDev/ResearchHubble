cd4da11a2d0c3d3a2807eca174db76a9e0dbdaf4
AdiKsOnDev <adilalizade13@gmail.com>
2023-10-02 18:04:35 +0400

    Query now works

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,22 +1,21 @@
 import React, { useState } from 'react';
 import { database } from '../firebase';
-import { collection } from 'firebase/firestore';
+import { collection, query, getDocs, where } from 'firebase/firestore';
 
 const SearchBox = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
 
   const handleSearch = async () => {
-    const projectsRef = collection(database, 'projects');
-
+    const projectsRef = collection(database, 'Projects');
+  
     // Perform a Firestore query to search for projects
-    const querySnapshot = query(query(citiesRef, where("Name", ">=", searchQuery)), where("Name", "<=", searchQuery));
-
+    const querySnapshot = await getDocs(query(projectsRef, where("Name", ">=", searchQuery), where("Name", "<=", searchQuery)));
+  
     const results = querySnapshot.docs.map((doc) => doc.data());
     setSearchResults(results);
-  };
-  // className="w-full h-full px-4 py-2 border rounded-l-md focus:outline-none text-xl font-semibold"
-  // className="px-4 py-2 h-full text-bone bg-grass font-semibold rounded-r-md focus:outline-none text-xl"
+    console.log(results);
+  };  
 
   return (
     <div className="container mx-auto p-4">
@@ -35,11 +34,11 @@ const SearchBox = () => {
           Search
         </button>
       </div>
-      <div>
+      <div className='flex flex-col justify-center items-center'>
         {searchResults.map((result, index) => (
-          <div key={index} className="mt-4">
-            <h2 className="text-lg font-semibold">{result.title}</h2>
-            <p>{result.description}</p>
+          <div key={index} className="mt-4 text-bone flex flex-col justify-center items-center bg-metal p-5 w-fit rounded-md">
+            <h2 className="text-lg font-semibold">{result.Name}</h2>
+            <a className='hover:text-sky duration-300' href={result.Link}>{result.Link}</a>
           </div>
         ))}
       </div>
