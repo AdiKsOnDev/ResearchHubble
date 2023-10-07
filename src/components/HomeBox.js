b7c5e044902c409e9b6510e036694ce0f3d7379f
AdiKsOnDev <adilalizade13@gmail.com>
2023-10-07 16:17:26 +0400

    Homepage has a User card now

--- src/components/HomeBox.js
+++ src/components/HomeBox.js
@@ -11,11 +11,11 @@ function HomeBox() {
   return ( 
     <div className="flex flex-row h-screen justify-center items-center">
       {/* User Card */}
-      <div className="bg-metal h-1/4 w-1/5 rounded-md flex flex-col justify-center items-center">
+      <div className="bg-metal h-1/4 min-h-fit min-w-fit w-1/3 rounded-md flex flex-col justify-center items-center">
         <h1 className="text-bone text-3xl font-semibold">{userName}</h1>
         <h2 className="text-bone text-sm">User's ID</h2>
 
-        <a href="" className="w-full border-t-2 border-bone mt-12 text-sky p-5 text-xl font-semibold hover:underline">Saved Projects</a>
+        <a href="" className="w-3/4 text-center border-t-2 border-bone mt-8 text-sky p-5 text-xl font-semibold hover:underline">Saved Projects</a>
       </div>
       
       {/* Recommendations */}
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,18 @@
 import { ReactComponent as ManuscriptSVG } from '../Assets/manuscript.svg';
 
-function Project(project, key) {
+function Project(prLink, prDescription, prName, prSaved) {
   return ( 
     <div className='flex flex-col justify-center items-center w-full'>
-      <div key={key} className="mt-4 text-bone flex flex-row justify-around items-center w-full bg-metal p-5">
+      <div key={prName} className="mt-4 text-bone flex flex-row justify-around items-center w-full bg-metal p-5">
         <div className='flex flex-col w-1/4'>
-          <a className="text-2xl font-semibold w-fit flex mb-2 text-sky hover:underline" href={project.Link}>{project.Name}</a>
+          <a className="text-2xl font-semibold w-fit flex mb-2 text-sky hover:underline" href={prLink}>{prName}</a>
           <p className="description text-bone font-thin"> {/* Set the width here */}
-            {project.Description}
+            {prDescription}
           </p>
 
           <div className='w-full'>
             <div className='mt-2 flex flex-row'>
-              <span className='flex flex-row items-center justify-center'><ManuscriptSVG className='h-5 w-5 mr-2 mt-2' /> <p className='font-semibold mr-3'>{project.Saved}</p></span>
+              <span className='flex flex-row items-center justify-center'><ManuscriptSVG className='h-5 w-5 mr-2 mt-2' /> <p className='font-semibold mr-3'>{prSaved}</p></span>
             </div>
           </div>
         </div>
