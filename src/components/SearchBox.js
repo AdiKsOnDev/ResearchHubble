cc29a5e22c885f14e35ea5f2d10e551e2dc1a370
AdiKsOnDev <adilalizade13@gmail.com>
2023-10-07 12:24:45 +0400

    Project saved n number of times

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,6 +3,8 @@ import { database } from '../firebase';
 import { collection, query, getDocs, where } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
 import Fuse from 'fuse.js';
+import { ReactComponent as ManuscriptSVG } from '../Assets/manuscript.svg';
+
 const SearchBox = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
@@ -45,12 +47,23 @@ const SearchBox = () => {
         </button>
       </div>
       <Link to="/Add-Project" className='text-bone mt-5 font-semibold text-lg hover:text-sky duration-300'>Can't Find your project? Add it yourself</Link>
-      <div className='flex flex-col justify-center items-center'>
+      <div className='flex flex-col justify center items-center bg-metal w-fit mt-5'>
         {searchResults != [] ? searchResults.map((result, index) => (
-          <div key={index} className="mt-4 text-bone flex flex-col justify-center items-center bg-metal p-5 w-full rounded-md">
-            <h2 className="text-2xl font-semibold">{result.Name}</h2>
-            <p className='mt-2'><span className='font-semibold'>Contributors:</span> {result.Contributors}</p>
-            <a href={result.Link}><button className='px-5 py-2 bg-grass font-semibold mt-5 rounded-md hover:px-7 duration-300'>Link</button></a>
+          <div className='flex flex-col justify-center items-center w-full'>
+            <div key={index} className="mt-4 text-bone flex flex-row justify-around items-center w-full bg-metal p-5">
+              <div className='flex flex-col w-1/4'>
+                <a className="text-2xl font-semibold w-fit flex mb-2 text-sky hover:underline" href={result.Link}>{result.Name}</a>
+                <p className="description text-bone font-thin"> {/* Set the width here */}
+                  {result.Description}
+                </p>
+                <div className='w-full'>
+                  <p className='mt-2 flex flex-row'>
+                    <icon className='flex flex-row items-center justify-center'><ManuscriptSVG className='h-5 w-5 mr-2 mt-2' /> <p className='font-semibold mr-3'>{result.Saved}</p></icon>
+                  </p>
+                </div>
+              </div>
+              <button className='flex flex-row justify-center items-center px-5 py-2 bg-grass font-semibold rounded-md hover:px-7 duration-300'><ManuscriptSVG className='h-5 w-5 mr-2'/>Save</button>
+            </div>
           </div>
         )) : ``}
       </div>
