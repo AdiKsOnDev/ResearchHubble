073d8834b1920c20be335f2c87aef18670cea3a5
AdiKsOnDev <adilalizade13@gmail.com>
2023-10-08 22:55:02 +0400

    Added top projects

--- src/components/HomeBox.js
+++ src/components/HomeBox.js
@@ -3,13 +3,16 @@ import { useState, useEffect } from 'react'
 import fetchUserData from "./scripts/fetchUserData";
 import recommendProjects from "./scripts/recommenderSystem";
 import Project from "./Project";
-function HomeBox() {
+import getTopProjects from "./scripts/getTopProjects"
 
+function HomeBox() {
   const [displayName, setDisplayName] = useState("Guest");
   const [description, setDescription] = useState("");
   const [userObj, setUserObj] = useState(null);
   let [loggedIn, setLoggedIn] = useState("");
   const [projects, setProjects] = useState([]); // Use state to store the projects.
+  const [topProjects, setTopProjects] = useState([]); // Use state to store the projects.
+
   useEffect(() => {
     // Set up a Firebase authentication state listener to detect changes in authentication status.
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
@@ -44,7 +47,7 @@ function HomeBox() {
       // Use async/await to wait for the promise to resolve.
       (async () => {
         const recommendedProjects = await recommendProjects(auth.currentUser.email, false);
-        console.log("The recommended projects are ",recommendedProjects);
+        console.log("The recommended projects are ", recommendedProjects);
         if (Array.isArray(recommendedProjects)) {
           setProjects(recommendedProjects); // Update the state with the projects.
         }
@@ -52,10 +55,22 @@ function HomeBox() {
     }
   }, [loggedIn]);
 
+  useEffect(() => {
+    // Use async/await to wait for the promise to resolve.
+    (async () => {
+      const result = await getTopProjects();
+      console.log("The TOP projects are ", result);
+
+      if (Array.isArray(result)) {
+        setTopProjects(result); // Update the state with the projects.
+      }
+    })();
+  }, [loggedIn]);
+
   return ( 
     <div className="flex flex-row h-screen justify-center mt-28">
       {/* User Card */}
-      <div className="mt-12 bg-metal h-1/4 min-h-fit min-w-fit w-1/4 mx-12 rounded-md flex flex-col justify-center items-center">
+      <div className="mt-12 bg-metal h-1/4 min-h-fit min-w-fit w-2/4 mx-12 rounded-md flex flex-col justify-center items-center">
         <h1 className="text-bone text-3xl font-semibold">{displayName}</h1>
         <h2 className="text-bone text-sm">User's ID</h2>
 
@@ -64,6 +79,7 @@ function HomeBox() {
       
       {/* Recommendations */}
       <div className='flex flex-col justify center items-center bg-metal w-fit rounded-md mt-12'>
+      <h1 className="my-10 text-bone font-semibold text-3xl">Perfect picks, just for you</h1>
         {projects.length !== 0 ? projects.map((recommendation) => (
           <div key={recommendation.project.Name} className='flex flex-col justify-center items-center w-full'>
             <Project prLink={recommendation.project.Link} prDescription={recommendation.project.Description} prName={recommendation.project.Name} prSaved={recommendation.project.Saved}></Project>
@@ -72,8 +88,13 @@ function HomeBox() {
       </div>
 
       {/* Trending projects */}
-      <div>
-        
+      <div className='flex flex-col justify center items-center bg-metal w-fit h-fit rounded-md mt-12 mx-12'>
+        <h1 className="my-10 text-bone font-semibold text-3xl">Top Projects</h1>
+        {topProjects.length !== 0 ? topProjects.map((top) => (
+          <div key={top.Name} className='flex flex-col justify-center items-center w-full'>
+            <Project prLink={top.Link} prDescription={top.Description} prName={top.Name} prSaved={top.Saved}></Project>
+          </div>
+        )) : ''}
       </div>
     </div>
   );
