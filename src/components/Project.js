35e9f2eaa21250aae91fa5525c5524500f346410
rachaelteresadias <rt2004@hw.ac.uk>
2023-10-08 18:18:22 +0400

    Save project added, saved state remembered. #14

--- src/components/Project.js
+++ src/components/Project.js
@@ -32,36 +32,36 @@
 
 //   const handleSave = async () => {
 //    // console.log("Handling save for result:", result);
-    
+
 //     // Add debug logs to check variables
 //     // console.log("result name is ", prName)
 //     // console.log("userObj:", userObj);
 //     // console.log("description:", description);
 //     // console.log("isSaved:", isSaved);
 //     // setIsSaved(savedProjects.some((savedProject) => savedProject.Name === prName));
-  
+
 //     const user = auth.currentUser;
 //     const userId = user ? user.email : "";
-  
+
 //     const userDocRef = userId ? doc(collection(database, 'Users'), userId) : "";
 //     setUserObj(await fetchUserData(userId));
-  
+
 //     if (!user) {
 //       // Handle the case where the user is not authenticated
 //       console.log("User is not authenticated");
 //       return;
 //     }
-  
+
 //     if (userObj === null) {
 //       // If userObj is null, set it to an empty object
 //       setUserObj({});
 //     }
-  
+
 //     // Check if userObj has a valid 'saved_projects' property
 //     if (!userObj.saved_projects) {
 //       userObj.saved_projects = [];
 //     }
-  
+
 //     // Check if 'description' is defined and parse it if it's a string
 //     // if (description) {
 //     //   try {
@@ -75,8 +75,6 @@
 //   // const userId = user ? user.email : "";
 
 //   // const userDocRef = userId? (await doc(collection(database, 'Users'), userId)) : "";
-
-
 
 //   // Reference to the user's document in Firestore
 
@@ -100,7 +98,7 @@
 //     );
 
 //     if (!isProjectSaved) {
-      
+
 //       const updatedSavedProjects = [
 //         ...userObj?.saved_projects,
 //         { projectName: prName },
@@ -109,9 +107,9 @@
 //       // Update the user's document with the updated "saved_projects" array
 //       await updateDoc(
 //         userDocRef,
-//         { 
+//         {
 //           "saved_projects": updatedSavedProjects },
-       
+
 //       );
 //       setIsSaved(true);
 //     }else{
@@ -121,9 +119,9 @@
 //           )
 //       await updateDoc(
 //         userDocRef,
-//         { 
+//         {
 //           "saved_projects": updatedSavedProjects },
-       
+
 //       );
 //       setIsSaved(false);
 //       console.log(isSaved);
@@ -131,7 +129,7 @@
 //   }
 
 //   if (isSaved) {
-    
+
 //     // setSavedProjects(
 //     //   savedProjects.filter(
 //     //     (savedProject) => savedProject.Name !== prName
@@ -153,7 +151,7 @@
 //     // Decrement the Saved property in Firebase
 //     const projectRef = doc(database, "Projects", prName);
 //     await updateDoc(projectRef, { Saved: prSaved + 1 });
-    
+
 //     await updateDoc(userDocRef, {'saved_projects': userObj?.saved_projects.filter(
 //       (savedProject) => savedProject.Name !== prName
 //     ) })
@@ -187,7 +185,7 @@
 // };
 // //console.log("Rendering Project component with props:", { prLink, prDescription, prName, prSaved });
 
-//   return ( 
+//   return (
 //     <div className='flex flex-col justify-center items-center w-full'>
 //       <div className="mt-4 text-bone flex flex-row justify-around items-center w-full bg-metal p-5">
 //         <div className='flex flex-col w-1/4'>
@@ -231,36 +229,40 @@
 
 // export default Project;
 
-import { ReactComponent as ManuscriptSVG } from '../Assets/manuscript.svg';
-import { ReactComponent as ManuscriptFilledSVG } from '../Assets/manuscriptfill.svg';
+import { ReactComponent as ManuscriptSVG } from "../Assets/manuscript.svg";
+import { ReactComponent as ManuscriptFilledSVG } from "../Assets/manuscriptfill.svg";
 import React, { useEffect, useState } from "react";
 import { auth, database } from "../firebase";
 import { collection, doc, updateDoc } from "firebase/firestore";
-import fetchUserData from './scripts/fetchUserData';
+import fetchUserData from "./scripts/fetchUserData";
 
 function Project({ prLink, prDescription, prName, prSaved }) {
   const [userObj, setUserObj] = useState({});
+  const [probJect, setProbject] = useState(
+    doc(collection(database, "Projects"), prName)
+  );
   const [isSaved, setIsSaved] = useState(false);
+  const [savedCount, setSavedCount] = useState(prSaved); // Added savedCount state
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
       if (user && user.email) {
-        // If the user is authenticated, load user data.
         const userData = await fetchUserData(user.email);
         setUserObj(userData || {});
       } else {
-        // If the user is not authenticated, set default values.
         setUserObj({});
       }
     });
 
-    // Cleanup the listener when the component unmounts.
     return () => unsubscribe();
   }, []);
 
   useEffect(() => {
-    // Check if the project is saved when the component mounts
-    setIsSaved(userObj?.saved_projects?.some(savedProject => savedProject.projectName === prName));
+    setIsSaved(
+      userObj?.saved_projects?.some(
+        (savedProject) => savedProject.projectName === prName
+      )
+    );
   }, [userObj, prName]);
 
   const handleSave = async () => {
@@ -268,51 +270,61 @@ function Project({ prLink, prDescription, prName, prSaved }) {
     const userId = user ? user.email : "";
 
     if (!user) {
-      // Handle the case where the user is not authenticated
       console.log("User is not authenticated");
       return;
     }
 
-    const userDocRef = doc(collection(database, 'Users'), userId);
+    const userDocRef = doc(collection(database, "Users"), userId);
     await fetchUserData(userId);
 
     if (!userObj) {
-      // If userObj is null, set it to an empty object
       setUserObj({});
     }
 
     const updatedSavedProjects = userObj.saved_projects || [];
-    const isProjectSaved = updatedSavedProjects.some(savedProject => savedProject.projectName === prName);
+    const isProjectSaved = updatedSavedProjects.some(
+      (savedProject) => savedProject.projectName === prName
+    );
 
     if (!isProjectSaved) {
       updatedSavedProjects.push({ projectName: prName });
+      setSavedCount(savedCount + 1); // Increment savedCount
+      setProbject(await doc(collection(database, "Projects"), prName));
+      updateDoc(probJect, { Saved: savedCount });
     } else {
-      // Remove the project if it's already saved
-      const index = updatedSavedProjects.findIndex(savedProject => savedProject.projectName === prName);
+      const index = updatedSavedProjects.findIndex(
+        (savedProject) => savedProject.projectName === prName
+      );
       if (index !== -1) {
         updatedSavedProjects.splice(index, 1);
+        setSavedCount(savedCount - 1); // Decrement savedCount
+        setProbject(await doc(collection(database, "Projects"), prName));
+        updateDoc(probJect, { Saved: savedCount });
       }
     }
 
-    // Update the user's document with the updated "saved_projects" array
     await updateDoc(userDocRef, { saved_projects: updatedSavedProjects });
     setIsSaved(!isSaved);
   };
 
   return (
-    <div className='flex flex-col justify-center items-center w-full'>
+    <div className="flex flex-col justify-center items-center w-full">
       <div className="mt-4 text-bone flex flex-row justify-around items-center w-full bg-metal p-5">
-        <div className='flex flex-col w-1/4'>
-          <a className="text-2xl font-semibold w-fit flex mb-2 text-sky hover:underline" href={prLink}>{prName}</a>
-          <p className="description text-bone font-thin">
-            {prDescription}
-          </p>
+        <div className="flex flex-col w-1/4">
+          <a
+            className="text-2xl font-semibold w-fit flex mb-2 text-sky hover:underline"
+            href={prLink}
+          >
+            {prName}
+          </a>
+          <p className="description text-bone font-thin">{prDescription}</p>
 
-          <div className='w-full'>
-            <div className='mt-2 flex flex-row'>
-              <span className='flex flex-row items-center justify-center'>
-                <ManuscriptSVG className='h-5 w-5 mr-2 mt-2' />
-                <p className='font-semibold mr-3'>{prSaved}</p>
+          <div className="w-full">
+            <div className="mt-2 flex flex-row">
+              <span className="flex flex-row items-center justify-center">
+                <ManuscriptSVG className="h-5 w-5 mr-2 mt-2" />
+                <p className="font-semibold mr-3">{savedCount}</p>{" "}
+                {/* Display savedCount */}
               </span>
             </div>
           </div>
