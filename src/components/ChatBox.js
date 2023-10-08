6d3ec9841654a54cd5a401a3b3236489f12ff579
AdiKsOnDev <adilalizade13@gmail.com>
2023-10-08 23:39:50 +0400

    Basic outline

---
+++ src/components/ChatBox.js
@@ -0,0 +1,12 @@
+function ChatBox() {
+  const [myMessages, setmyMessages] = useState([]);
+  const [llmMessages, setllmMessages] = useState([]);
+
+  return ( 
+    <main className="w-max bg-metal h-max">
+      
+    </main>
+  );
+}
+
+export default ChatBox;
