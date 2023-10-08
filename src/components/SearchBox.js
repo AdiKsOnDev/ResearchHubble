ff5609eaad469dd4c64ff0b296d952f187769174
rachaelteresadias <rt2004@hw.ac.uk>
2023-10-08 12:58:08 +0400

    update

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,11 @@ import { collection, query, getDocs, where } from "firebase/firestore";
 import { Link } from "react-router-dom";
 import Fuse from "fuse.js";
 import { ReactComponent as ManuscriptSVG } from "../Assets/manuscript.svg";
+import { ReactComponent as ManuscriptFilled } from "../Assets/manuscriptfill.svg"; // Filled SVG
+import { doc, updateDoc } from "firebase/firestore";
+import { auth } from "../firebase";
+import { getDoc, setDoc } from "firebase/firestore";
+import { deleteDoc } from "firebase/firestore";
 
 const SearchBox = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -36,30 +41,95 @@ const SearchBox = () => {
     setSearchResults(results);
   };
 
-  const handleSave = (result) => {
-    // Check if the project is already saved
-    if (
-      savedProjects.some((savedProject) => savedProject.Name === result.Name)
-    ) {
+  const handleSave = async (result) => {
+    const isSaved = savedProjects.some(
+      (savedProject) => savedProject.Name === result.Name
+    );
+
+    // Get the current user
+    const user = auth.currentUser;
+
+    if (!user) {
+      // Handle the case where the user is not authenticated
+      console.log("User is not authenticated");
+      return;
+    }
+
+    const userId = user.email;
+
+    // Reference to the user's document in Firestore
+    const userDocRef = doc(database, "Users", userId);
+
+    // Fetch the user's document
+    const userDocSnapshot = await getDoc(userDocRef);
+    const userData = userDocSnapshot.data();
+
+    if (!userData || !userData["saved-projects"]) {
+      // If userData or "saved-projects" array doesn't exist, create it
+      const newSavedProjects = [result.ref];
+
+      // Update the user's document with the new "saved-projects" array
+      await setDoc(
+        userDocRef,
+        { "saved-projects": newSavedProjects },
+        { merge: true }
+      );
+    } else {
+      // If "saved-projects" array exists, check if the project is already saved
+      const isProjectSaved = userData["saved-projects"].some(
+        (savedProject) => savedProject.projectName === result.Name
+      );
+
+      if (!isProjectSaved) {
+        // If the project is not saved, add it to the "saved-projects" array
+        const updatedSavedProjects = [
+          ...userData["saved-projects"],
+          { projectName: result.Name },
+        ];
+
+        // Update the user's document with the updated "saved-projects" array
+        await setDoc(
+          userDocRef,
+          { "saved-projects": updatedSavedProjects },
+          { merge: true }
+        );
+      }
+    }
+
+    if (isSaved) {
       // Remove the project from savedProjects
       setSavedProjects(
         savedProjects.filter(
           (savedProject) => savedProject.Name !== result.Name
         )
       );
-      // Update the Saved property in searchResults
+
+      // Decrement the Saved property and update in searchResults
       const updatedSearchResults = searchResults.map((searchResult) => {
         if (searchResult.Name === result.Name) {
-          return { ...searchResult, Saved: searchResult.Saved - 1 };
+          return {
+            ...searchResult,
+            Saved: searchResult.Saved - 1,
+            IsSaved: false,
+          };
         }
         return searchResult;
       });
       setSearchResults(updatedSearchResults);
+
+      // Decrement the Saved property in Firebase
+      const projectRef = doc(database, "Projects", result.Name);
+      await updateDoc(projectRef, { Saved: result.Saved - 1 });
     } else {
       // Increment the Saved property and add the project to savedProjects
-      const updatedResult = { ...result, Saved: result.Saved + 1 };
+      const updatedResult = {
+        ...result,
+        Saved: result.Saved + 1,
+        IsSaved: true,
+      };
       setSavedProjects([...savedProjects, updatedResult]);
-      // Update the Saved property in searchResults
+
+      // Increment the Saved property in searchResults
       const updatedSearchResults = searchResults.map((searchResult) => {
         if (searchResult.Name === result.Name) {
           return updatedResult;
@@ -67,6 +137,10 @@ const SearchBox = () => {
         return searchResult;
       });
       setSearchResults(updatedSearchResults);
+
+      // Increment the Saved property in Firebase
+      const projectRef = doc(database, "Projects", result.Name);
+      await updateDoc(projectRef, { Saved: result.Saved + 1 });
     }
   };
 
@@ -126,12 +200,17 @@ const SearchBox = () => {
                     className="flex flex-row justify-center items-center px-5 py-2 bg-grass font-semibold rounded-md hover:px-7 duration-300"
                     onClick={() => handleSave(result)}
                   >
-                    <ManuscriptSVG className="h-5 w-5 mr-2" />
-                    {savedProjects.some(
-                      (savedProject) => savedProject.Name === result.Name
-                    )
-                      ? "Unsave"
-                      : "Save"}
+                    {result.IsSaved ? (
+                      <>
+                        <ManuscriptFilled className="h-5 w-5 mr-2" />
+                        Unsave
+                      </>
+                    ) : (
+                      <>
+                        <ManuscriptSVG className="h-5 w-5 mr-2" />
+                        Save
+                      </>
+                    )}
                   </button>
                 </div>
               </div>
